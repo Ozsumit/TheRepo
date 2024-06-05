f289d35f10bd88e4903d5fb041a6e1c612812f16
Ozsumit <129023326+Ozsumit@users.noreply.github.com>
2024-06-05 15:21:56 +0545

    Add files via upload

--- site.js
+++ site.js
@@ -1,8 +1,8 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      console.log('Service Worker Registered', registration);
-    }).catch(function(error) {
-      console.log('Service Worker registration failed:', error);
-    });
-  }
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+//       console.log('Service Worker Registered', registration);
+//     }).catch(function(error) {
+//       console.log('Service Worker registration failed:', error);
+//     });
+//   }
   
